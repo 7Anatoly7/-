eee99d94d67315da70d6677fdaea2a70e4ccfbe4
7Anatoly7 <122594490+7Anatoly7@users.noreply.github.com>
2023-03-16 20:03:04 +0300

    Update Pole_chudes_game.py

--- Pole_chudes_game.py
+++ Pole_chudes_game.py
@@ -26,9 +26,23 @@ while True:
     # Проверка, найдены ли все буквы в слове
     if set(hidden_word) == find_letters:
         print('Ура, победа! Вы угадали слово!')
-        break
+        question2 = input('Хотите другое загаданное слово?(да/нет) ').lower()
+        is_out = False
+        while True:
+            if question2 == 'да':
+                hidden_word = random.choice(words)
+                break
+            elif question2 == 'нет':
+                print('До свидания!')
+                is_out = True
+                break
+            else:
+                print('Команда введена неверно. Попробуйте заново')
+                question2 = input('Хотите другое загаданное слово?(да/нет) ').lower()
+        if is_out:
+            break
     # Ввод буквы или всего слова целиком
-    players_letter = input('Введите букву или всё слово: ').lower()
+    players_letter = input('Введите букву или всё слово: ').lower().strip()
     # Проверка, введена ли одна буква и буква ли это вообще
     if len(players_letter) == 1 and 'а' <= players_letter <= 'я':
         # Проверка, есть ли буква в слове и не была ли она уже названа
@@ -44,29 +58,76 @@ while True:
         # Проверка, угадано ли слово
         if players_letter == hidden_word:
             print('Ура, победа! Вы угадали слово!')
-            break
+            question2 = input('Хотите другое загаданное слово?(да/нет) ').lower()
+            is_out = False
+            while True:
+                if question2 == 'да':
+                    hidden_word = random.choice(words)
+                    break
+                elif question2 == 'нет':
+                    print('До свидания!')
+                    is_out = True
+                    break
+                else:
+                    print('Команда введена неверно. Попробуйте заново')
+                    question2 = input('Хотите другое загаданное слово?(да/нет) ').lower()
+            if is_out:
+                break
+        elif ' ' in players_letter:
+            print('Загадано только одно слово')
+        elif len(players_letter) < len(hidden_word):
+            print('Вводите по одной букве')
+        elif len(players_letter) > len(hidden_word):
+            print('Введённая строка превышает длину загаданного слова')
         else:
             print('Увы, вы не угадали слово :(')
     # Проверка на то, может ли это быть командой
     elif players_letter[0] == '!':
         # Проверка, есть ли эта команда в списке команд
-        if players_letter[1:] in ['help', 'use', 'remain', 'continue', 'exit']:
+        if players_letter[1:] in ['help', 'use', 'remain', 'surrender', 'exit']:
             # Выполнение команд
             if players_letter[1:] == 'help':
                 print('!help - вывод текущей справки\n'
                       '!use - показать использованные буквы\n'
                       '!remain - показать оставшиеся буквы\n'
-                      '!continue - пропустить ход\n'
+                      '!surrender - сдаться\n'
                       '!exit - выйти из игры')
             elif players_letter[1:] == 'use':
                 print(used_letters)
             elif players_letter[1:] == 'remain':
                 print(alphabet - used_letters)
-            elif players_letter[1:] == 'continue':
-                continue
+            elif players_letter[1:] == 'surrender':
+                question1 = input('Вы уверены, что хотите сдаться? ').lower()
+                if question1 == 'да':
+                    print('Загаданное слово:', hidden_word)
+                    question2 = input('Хотите другое загаданное слово?(да/нет) ').lower()
+                    is_out = False
+                    while True:
+                        if question2 == 'да':
+                            hidden_word = random.choice(words)
+                            break
+                       elif question2 == 'нет':
+                            print('До свидания!')
+                            is_out = True
+                            break
+                        else:
+                            print('Команда введена неверно. Попробуйте заново')
+                            question2 = input('Хотите другое загаданное слово?(да/нет) ').lower()
+                    if is_out:
+                        break
+                elif question1 == 'нет':
+                    continue
+                else:
+                    print('Попробуйте заново')
             elif players_letter[1:] == 'exit':
-                print('До свидания!')
-                break
+                question1 = input('Вы уверены, что хотите выйти? ').lower()
+                if question1 == 'да':
+                    print('До свидания!')
+                    break
+                elif question1 == 'нет':
+                    continue
+                else:
+                    print('Попробуйте заново')
         else:
             print('Такой команды не существует')
     else:
